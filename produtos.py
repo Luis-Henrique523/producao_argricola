34743bf8420b9f6799f1abdd038ed4bfa94ec146
Luis-Henrique523 <sales.luis@academico.ifpb.edu.br>
2025-07-18 14:55:34 +0000

    tabelas dos produtos

---
+++ produtos.py
@@ -0,0 +1,34 @@
+c_banana = {'ara': 4.800,
+             'mudas': 6.0,
+              'insumos': 5.600,
+               'mao_obra': 6.000,
+                'energia': 160  }
+p_banana = [3.00, 500, 1500]
+
+c_abacaxi = {'ara': 4.800,
+             'mudas': 2.151,
+             'insumos': 7.500,
+             'mao_obra': 6200,
+             'energia': 187}
+p_abacaxi = [5.00, 1000, 5000]
+
+c_uva ={'ara': 15.000,       
+    'mudas': 15.500,     
+    'insumos': 14.385,   
+    'mao_obra': 17.070,  
+    'energia': 2.550 } 
+p_uva =[9.00, 700, 6300] 
+
+c_melancia = {'ara': 14.000,       
+    'mudas': 11.500,     
+    'insumos': 12.000,   
+    'mao_obra': 17.000,  
+    'energia': 2.000 }
+p_melancia = [2.00, 1200, 2400]  
+
+c_tangerina = {'ara': 11.000,       
+    'mudas': 9.500,     
+    'insumos': 10.000,   
+    'mao_obra': 12.000,  
+    'energia': 1.800 }
+
